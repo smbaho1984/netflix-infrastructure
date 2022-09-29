26e277951058600667cc8573f7172e5476ea8597
unknown <stephanembaho@gmail.com>
2022-09-29 12:54:40 -0400

    issue resolved

---
+++ s3/s3.tf
@@ -0,0 +1,10 @@
+resource "aws_s3_bucket" "b" {
+  bucket = var.bucket_name
+
+  tags = {
+    Name        = "my bucket"
+    Environment = "Dev"
+  }
+}
+
+
---
+++ s3/variable.tf
@@ -0,0 +1,4 @@
+variable "bucket_name" {
+    default = "netflix-bucket-1984"
+
+}
